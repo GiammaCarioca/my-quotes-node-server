284c9b13855482a6177238407faacfff8ff11f43
giammacarioca <giammattey@gmail.com>
2022-02-23 20:41:09 -0300

    dotenv

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,16 @@
+require('dotenv').config()
+
 const admin = require('firebase-admin')
 
+const serviceAccount = {
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+}
+
 const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert({
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-  }),
+  credential: admin.credential.cert(serviceAccount),
 })
 
 const firebaseDB = admin.firestore()
