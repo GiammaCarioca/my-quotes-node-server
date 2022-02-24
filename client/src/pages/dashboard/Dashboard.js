bbcc930bcc93ee596ad8b6300ad55375041f28f1
giammacarioca <giammattey@gmail.com>
2022-02-24 15:15:29 -0300

    conditionally displaying components

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,13 +1,24 @@
+import { useState } from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
+
 import './Dashboard.css'
 
 import QuotesList from '../../components/QuotesList'
 import UsersList from '../../components/UsersList'
 
 export default function Dashboard() {
+  const [isAdmin, setIsAdmin] = useState(null)
+
+  const { user } = useAuthContext()
+
+  user
+    .getIdTokenResult()
+    .then((idTokenResult) => setIsAdmin(idTokenResult.claims.admin))
+
   return (
     <>
       <h2>Dashboard</h2>
-      <UsersList />
+      {isAdmin && <UsersList />}
       <QuotesList />
     </>
   )
